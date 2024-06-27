dec1e76628fbc2a83dd11bdddb5a2f29dc7035f4
shengxic <124843788+SeanXC@users.noreply.github.com>
2024-06-27 17:15:59 +0800

    upload current project

---
+++ reggie_take_out/src/main/java/com/itheima/reggie/common/MyMetaObjecthandler.java
@@ -0,0 +1,30 @@
+package com.itheima.reggie.common;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
+
+@Slf4j
+public class MyMetaObjecthandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("公共字段自动填充");
+        log.info(metaObject.toString());
+        metaObject.setValue("createTime", LocalDateTime.now());
+        metaObject.setValue("updateTime", LocalDateTime.now());
+//        metaObject.setValue("createUser", new Long(1));
+//        metaObject.setValue("updateUser", new Long(1));
+    }
+
+    /**
+     * 更新操作，自动填充
+     * @param metaObject
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("公共字段自动填充[update]...");
+        log.info(metaObject.toString());
+    }
+}
