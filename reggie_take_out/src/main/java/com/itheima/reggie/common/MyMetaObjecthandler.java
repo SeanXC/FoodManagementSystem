864475a5b75eef933fca5934f358cc72960bf976
shengxic <124843788+SeanXC@users.noreply.github.com>
2024-06-28 18:07:19 +0800

    business development D4

--- reggie_take_out/src/main/java/com/itheima/reggie/common/MyMetaObjecthandler.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/common/MyMetaObjecthandler.java
@@ -6,16 +6,21 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 
+@Component
 @Slf4j
 public class MyMetaObjecthandler implements MetaObjectHandler {
+    /**
+     * 插入操作，自动填充
+     * @param metaObject
+     */
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("公共字段自动填充");
         log.info(metaObject.toString());
         metaObject.setValue("createTime", LocalDateTime.now());
         metaObject.setValue("updateTime", LocalDateTime.now());
-//        metaObject.setValue("createUser", new Long(1));
-//        metaObject.setValue("updateUser", new Long(1));
+        metaObject.setValue("createUser", BaseContext.getCurrentId());
+        metaObject.setValue("updateUser", BaseContext.getCurrentId());
     }
 
     /**
@@ -26,5 +31,11 @@ public class MyMetaObjecthandler implements MetaObjectHandler {
     public void updateFill(MetaObject metaObject) {
         log.info("公共字段自动填充[update]...");
         log.info(metaObject.toString());
+
+        long id = Thread.currentThread().getId();
+        log.info("线程id为：{}",id);
+
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        metaObject.setValue("updateUser", BaseContext.getCurrentId());
     }
 }
